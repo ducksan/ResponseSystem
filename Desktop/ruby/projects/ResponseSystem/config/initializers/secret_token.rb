d63932d804c79094f4c1aa68b4bcc6486c178f14
Ducksan Cho <cdsjj@paran.com>
2011-09-02 09:58:16 +1200

    Initial commit test

---
+++ Desktop/ruby/projects/ResponseSystem/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResponseSystem::Application.config.secret_token = '7befc87d558edc8dbe6c2b6c7e43cf628bb012396b2a80c043d26cc41fd6d2a477a33ebfe49e7ad945e422574a693e893a58af5c97f8531db40666ec29d05283'
